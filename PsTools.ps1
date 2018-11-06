4dc672d13de9850f47edc27bbcbc3ceaeebf5913
Mentaleak <mentaleak@gmail.com>
2018-11-06 18:03:48 -0500

    Added PsTools.ps1
    Changed functions:
    Get-PSTool_functions
    set-PSTool_WebSecurity

---
+++ PsTools.ps1
@@ -0,0 +1,29 @@
+function set-PSTool_WebSecurity () {
+	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+}
+
+function Get-PSTool_functions () {
+	param(
+		[Parameter(mandatory = $true)] [string]$filePath
+	)
+
+	$file = Get-ChildItem $filePath
+	$oldarray = Get-ChildItem function:\
+	$acceptableExtensions = @(".dll",".ps1",".psm1",".psd1",".cdxml",".xaml")
+
+	if ($acceptableExtensions.Contains($file[0].Extension)) {
+		Import-Module $($file.FullName)
+		$newarray = Get-ChildItem function:\
+		$functions = ($newarray | Where-Object { $oldarray -notcontains $_ })
+		Remove-Module $($file.BaseName)
+		return $functions
+	}
+
+	else {
+		Write-Host "$($file.FullName) is not a Powershell File"
+		return @()
+
+	}
+
+
+}
