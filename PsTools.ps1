12826b496d106e3717f092b099abd75d0cedc3cb
Mentaleak <mentaleak@gmail.com>
2018-11-12 14:03:24 -0500

    Modified PsTools.ps1
    Changed functions:
    Get-PSTool_functions
    get-PSTool_usedCommands

--- PsTools.ps1
+++ PsTools.ps1
@@ -29,75 +29,75 @@ function Get-PSTool_functions () {
 }
 
 
-function get-PSTool_usedCommands(){
-    param(
-		    [Parameter(mandatory = $true)] [string]$filePath
-	    )
-    $file=get-item -Path $filePath
-    $fileData = get-content $filePath
-    $commandList= Get-Command | where{$_.Name -notmatch ":" -and $_.Source -notlike "Microsoft.Powershell.*" -and $_.Source -ne ""}
-    $mehCommandList = get-command | where {$_.Name -notmatch "-"}
-    $FoundCommands=@()
-    $mehFoundCommands=@()
-    $activeline=0
-    foreach($line in $fileData)
-    {
-    $activeline++
-    Write-Progress -Status "Reading $($file.name)" -activity "Line $activeline / $($fileData.count)" -PercentComplete (100*($activeline/$fileData.count))
-        #write-host $line
-        $upline=$line.ToUpper()
-        #write-host $upline
-        foreach($command in $commandList){
-            $commandName="$($command.name.ToUpper())"
-            #write-host $upline
-            if($upline -match [regex]::Escape($commandName))
-            {
-                
-                if(("$($upline)"[(("$($upline)".IndexOf("$commandName"))+"$commandName".Length)] -notmatch '^[a-zA-Z0-9]+$') -and ("$($upline)"[("$($upline)".IndexOf("$commandName"))-1] -notmatch '^[a-zA-Z0-9]+$'))
-                {
-                if ($mehCommandList| where{$_.name -eq $command.name} ){
-                #write-host "$($command.Name): $line" -ForegroundColor "White"
+function get-PSTool_usedCommands () {
+	param(
+		[Parameter(mandatory = $true)] [string]$filePath
+	)
+	$file = Get-Item -Path $filePath
+	$fileData = Get-Content $filePath
+	$commandList = Get-Command | Where-Object { $_.Name -notmatch ":" -and $_.Source -notlike "Microsoft.Powershell.*" -and $_.Source -ne "" }
+	$mehCommandList = Get-Command | Where-Object { $_.Name -notmatch "-" }
+	$FoundCommands = @()
+	$mehFoundCommands = @()
+	$activeline = 0
+	foreach ($line in $fileData)
+	{
+		$activeline++
+		Write-Progress -Status "Reading $($file.name)" -Activity "Line $activeline / $($fileData.count)" -PercentComplete (100 * ($activeline / $fileData.count))
+		#write-host $line
+		$upline = $line.ToUpper()
+		#write-host $upline
+		foreach ($command in $commandList) {
+			$commandName = "$($command.name.ToUpper())"
+			#write-host $upline
+			if ($upline -match [regex]::Escape($commandName))
+			{
 
-                    if($mehFoundCommands | where{$_.CommandName -eq "$($command.Name)"})
-                    {
-                      ($mehFoundCommands | where{$_.CommandName -eq "$($command.Name)"}).lines+="`n$($line.trim())"
-                    }
-                    else{
-                        $CommandObject = [PSCustomObject]@{
-                        CommandName= $command.Name
-                        Module = $command.source 
-                        Version = $command.version
-                        Lines = "$($line.trim())"
-                        }
-                        $mehFoundCommands+=$CommandObject
-                    }
-                
-                }else{
-                #write-host "$($command.Name): $line" -ForegroundColor "Green"
-                 if($FoundCommands | where{$_.CommandName -eq "$($command.Name)"})
-                    {
-                      ($FoundCommands | where{$_.CommandName -eq "$($command.Name)"}).lines+="`n$($line.trim())"
-                    }
-                    else{
-                        $CommandObject = [PSCustomObject]@{
-                        CommandName= $command.Name
-                        Module = $command.source 
-                        Version = $command.version
-                        Lines = "$($line.trim())"
-                        }
-                        $FoundCommands+=$CommandObject
-                    }
-                }
+				if (("$($upline)"[(("$($upline)".IndexOf("$commandName")) + "$commandName".Length)] -notmatch '^[a-zA-Z0-9]+$') -and ("$($upline)"[("$($upline)".IndexOf("$commandName")) - 1] -notmatch '^[a-zA-Z0-9]+$'))
+				{
+					if ($mehCommandList | Where-Object { $_.Name -eq $command.Name }) {
+						#write-host "$($command.Name): $line" -ForegroundColor "White"
 
-                }
-               
-            }
-        }
-    }
-    Write-Progress -Activity "Complete" -Completed
-    return [PSCustomObject]@{
-                        GoodCommands = $FoundCommands
-                        MehCommands = $mehFoundCommands
-                        }
+						if ($mehFoundCommands | Where-Object { $_.CommandName -eq "$($command.Name)" })
+						{
+							($mehFoundCommands | Where-Object { $_.CommandName -eq "$($command.Name)" }).lines += "`n$($line.trim())"
+						}
+						else {
+							$CommandObject = [pscustomobject]@{
+								CommandName = $command.Name
+								Module = $command.Source
+								Version = $command.Version
+								lines = "$($line.trim())"
+							}
+							$mehFoundCommands += $CommandObject
+						}
+
+					} else {
+						#write-host "$($command.Name): $line" -ForegroundColor "Green"
+						if ($FoundCommands | Where-Object { $_.CommandName -eq "$($command.Name)" })
+						{
+							($FoundCommands | Where-Object { $_.CommandName -eq "$($command.Name)" }).lines += "`n$($line.trim())"
+						}
+						else {
+							$CommandObject = [pscustomobject]@{
+								CommandName = $command.Name
+								Module = $command.Source
+								Version = $command.Version
+								lines = "$($line.trim())"
+							}
+							$FoundCommands += $CommandObject
+						}
+					}
+
+				}
+
+			}
+		}
+	}
+	Write-Progress -Activity "Complete" -Completed
+	return [pscustomobject]@{
+		GoodCommands = $FoundCommands
+		MehCommands = $mehFoundCommands
+	}
 
 }
